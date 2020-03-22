64c05288c2a3115c2babe2c0323a5637753bc7b3
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-03-22 13:20:40 -0400

    Set up Webpack

---
+++ src/index.jsx
@@ -0,0 +1,1 @@
+require("file-loader?name=[name].[ext]!html-minify-loader!./index.html");
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	entry: "./src/index.jsx",
+	mode: process.env.WEBPACK_MODE || "production",
+	module: {
+		rules: [{
+			test: /\.jsx$/,
+			use: "jsx-loader",
+		}],
+	},
+	output: {
+		filename: "index.js",
+		path: __dirname + "/dist",
+	},
+};
