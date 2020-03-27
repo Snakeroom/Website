7fd449c716f9a37a2205f404f3698156c147d947
Oden <hello@odensc.me>
2020-03-27 12:48:13 -0700

    Add alt to logo

--- components/logo.js
+++ components/logo.js
@@ -1,5 +1,7 @@
 import logoSvg from "../public/logo.svg";
 
-const Logo = ({ className }) => <img className={className} src={logoSvg} />;
+const Logo = ({ className }) => (
+	<img className={className} src={logoSvg} alt="a drawing of a green snake" />
+);
 
 export default Logo;
