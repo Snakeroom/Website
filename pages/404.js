d47f1891834c95ce8d8ccb31813934703265a283
Oden <hello@odensc.me>
2020-03-27 10:28:10 -0700

    Add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import Logo from "../public/logo.svg";
+
+const StyledLogo = styled(Logo)`
+	filter: grayscale();
+	margin-top: 1rem;
+	width: 8rem;
+`;
+
+const Container = styled.div`
+	display: flex;
+	flex: 1;
+	justify-content: center;
+`;
+
+const Title = styled.h1`
+	align-items: center;
+	align-self: center;
+	color: ${(props) => props.theme.colors.muted};
+	display: flex;
+	flex-direction: column;
+	font-weight: normal;
+`;
+
+export default () => (
+	<Container>
+		<Title>
+			Page not found. <StyledLogo />
+		</Title>
+	</Container>
+);
