fd1b74a0d55901c4213e936e9899652b6a026a9c
Oden <hello@odensc.me>
2020-03-27 11:41:15 -0700

    Add hero container to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import styled from "styled-components";
 
+const Hero = styled.div``;
+
 const Blurb = styled.p`
 	font-size: 2rem;
-	text-align: right;
 	margin-left: auto;
 	max-width: 28rem;
+	text-align: right;
 `;
 
 export default () => (
-	<Blurb>
-		<strong>The Snakeroom</strong> is an organization dedicated to solving
-		and discussing <strong>Reddit's</strong> yearly{" "}
-		<strong>April Fools events</strong> and official Reddit-run{" "}
-		<strong>ARGs</strong> — both before and while they happen.
-	</Blurb>
+	<Hero>
+		<Blurb>
+			<strong>The Snakeroom</strong> is an organization dedicated to
+			solving and discussing <strong>Reddit's</strong> yearly{" "}
+			<strong>April Fools events</strong> and official Reddit-run{" "}
+			<strong>ARGs</strong> — both before and while they happen.
+		</Blurb>
+	</Hero>
 );
