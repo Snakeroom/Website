9627924eb76f4c4f668824907d11a9d06d564284
Henry Le Grys <henry@legrys.info>
2020-10-20 20:33:55 +0100

    List user's tokens on Sneknet page & consolidate style

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
 import Head from "next/head";
-import styled from "styled-components";
-
-const Hero = styled.div``;
-
-const Blurb = styled.p`
-	font-size: 2rem;
-	margin-left: auto;
-	max-width: 28rem;
-	text-align: right;
-`;
+import { Blurb, Hero } from "../lib/common-style";
 
 export default () => (
 	<>
@@ -18,7 +9,7 @@ export default () => (
 		<Hero>
 			<Blurb>
 				<strong>The Snakeroom</strong> is an organization dedicated to
-				solving and discussing <strong>Reddit's</strong> yearly{" "}
+				solving and discussing <strong>Reddit&apos;s</strong> yearly{" "}
 				<strong>April Fools events</strong> and official Reddit-run{" "}
 				<strong>ARGs</strong> — both before and while they happen.
 			</Blurb>
--- pages/sneknet.js
+++ pages/sneknet.js
@@ -1,42 +1,90 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { makeApiRequest } from "../lib/api";
+import { StateContext } from "../lib/state";
+import { Box, PageTitle } from "../lib/common-style";
 
-const BoldButton = styled.a`
-	background: none;
-	border: 0;
-	cursor: pointer;
-	outline: 0;
-	display: block;
-
-	text-decoration: unset;
-	font-size: 2rem;
-	text-align: center;
-
-	color: ${(props) => props.theme.colors.primary};
-	background-color: ${(props) => props.theme.colors.accent};
-
-	padding: 8px;
-	border-radius: 16px;
-	max-width: 50%;
-	margin-left: 25%;
-
-	.svg-inline--fa {
-		margin-right: 0.75rem;
-		height: 2rem;
-		width: 2rem;
-	}
+const Content = styled.div`
+	font-size: 1rem;
 `;
 
-export default () => (
-	<>
-		<Head>
-			<title>Sneknet - The Snakeroom</title>
-		</Head>
-		<BoldButton href="https://api.snakeroom.org/identity/reddit/login?return_to=https%3A%2F%2Fsnakeroom.org%2Fsneknet">
-			<FontAwesomeIcon icon={faLock} />
-			Log in to Sneknet
-		</BoldButton>
-	</>
+const TokenList = styled.table`
+	margin-top: 1rem;
+	line-height: 1.5rem;
+`;
+
+const Row = styled.tr`
+	text-decoration: ${(props) => (props.active ? "none" : "line-through")};
+`;
+
+const TokenField = styled.td`
+	padding: 0 10px;
+`;
+
+const Token = ({ token }) => (
+	<Row active={token.active}>
+		<TokenField>{token.name}</TokenField>
+		<TokenField>
+			<code>{token.truncated_value}...</code>
+		</TokenField>
+		<TokenField>{token.whitelisted_address}</TokenField>
+	</Row>
 );
+
+export default () => {
+	const [tokens, setTokens] = useState([]);
+
+	useEffect(() => {
+		makeApiRequest("/sneknet/tokens")
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.tokens) {
+					setTokens(data.tokens);
+				}
+			});
+	}, []);
+
+	if (!StateContext._currentValue || !StateContext._currentValue.user) {
+		return (
+			<Box>
+				<h1>About Sneknet</h1>
+				<p>
+					Sneknet is the live component of our API, handling websocket
+					connections and tokens.
+				</p>
+				<p>Sign in to see your tokens!</p>
+			</Box>
+		);
+	}
+
+	return (
+		<>
+			<Head>
+				<title>Sneknet - The Snakeroom</title>
+			</Head>
+			<PageTitle>Sneknet</PageTitle>
+			<Content>
+				<h2>Tokens</h2>
+				<p>
+					Ask an admin for the full token if you&apos;ve misplaced
+					yours.
+				</p>
+				<TokenList>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Token</th>
+							<th>Whitelisted Address</th>
+						</tr>
+					</thead>
+					<tbody>
+						{tokens.map((token) => (
+							<Token key={token.name} token={token} />
+						))}
+					</tbody>
+				</TokenList>
+			</Content>
+		</>
+	);
+};
