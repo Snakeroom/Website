e8a802cfa01ebffec004d90c2f8fc1700d7c7aff
Megadynamite <megadynamitepublic@gmail.com>
2020-10-23 01:21:40 -0600

    Updated history.js Updated after review by @odensc

--- pages/history.js
+++ pages/history.js
@@ -12,52 +12,51 @@ export default () => (
 			<title>History - The Snakeroom</title>
 		</Head>
 		<Body>
-			<p>
-				<strong>April 2020 - Imposter</strong>
-				<br />
-				Imposter is a Turing Test, the player receives four messages
-				from Redditors and one from a bot and must determine which
-				message is the bot. Sneknet V3 uses a database of previously
-				collected answers to show the user if each answer is a human or
-				a bot. It also automatically selects if the correct answer is
-				known.
-				<br />
-				<strong>August 2019 - RPAN</strong>
-				<br />
-				RPAN is a streaming event with a cap on streamers and length.
-				Snakeroom creates scripts that snipe slots and allow the user to
-				broadcast from an obs stream, bypassing the intended limitation
-				of streaming from a mobile device and allowing Snakeroom members
-				to create &quot;Place In RPAN&quot; and &quot;RPAN Plays
-				Pokemon&quot;. They also begin streaming before the event is
-				live, though quickly shut down by Reddit admins.
-				<br />
-				<strong>April 2019 - Sequence</strong>
-				<br />
-				During sequence, users upload and vote on gifs. The most upvoted
-				gifs are strung together into a video. The Snakeroom creates
-				Sneknet V2. The new extension accepts a link to a gif on the
-				backend and then selects this gif for all users running the
-				extension, locking in gif choices without chancing success on
-				the whims of Reddit.
-				<br />
-				<strong>April 2018 - Circle of Trust</strong>
-				<br />
-				In Circle of Trust, users create a circle. Each circle has a
-				passcode, a user not in the circle can use the passcode to join
-				or betray. Joining enlarges the circle, while betraying ends it.
-				The Snakeroom creates the first Sneknet, a browser extension to
-				automate the event. The extension automatically joins a circle
-				from a command sent on the backend and adds anyone running the
-				extension before it can be betrayed.
-				<br />
-				<strong>March 2018 - Snakeroom Genesis</strong>
-				<br />A group founds The Snakeroom Alliance after Reddit
-				discovers r/snakeroomtest and many believe it to be part of the
-				2018 Reddit Fools’ event. The Snakeroom Alliance is shortened to
-				The Snakeroom and a development team forms to interact with the
-				event.
-			</p>
+			<strong>April 2020 - Imposter</strong>
+			<br />
+			Imposter is a Turing Test, the player receives four messages from
+			Redditors and one from a bot and must determine which message is the
+			bot. Sneknet V3 uses a database of previously collected answers to
+			show the user if each answer is a human or a bot. It also
+			automatically selects if the correct answer is known.
+			<br />
+			<br />
+			<strong>August 2019 - RPAN</strong>
+			<br />
+			RPAN is a streaming event with a cap on streamers and length.
+			Snakeroom creates scripts that snipe slots and allow the user to
+			broadcast from an obs stream, bypassing the intended limitation of
+			streaming from a mobile device and allowing Snakeroom members to
+			create &quot;Place In RPAN&quot; and &quot;RPAN Plays Pokemon&quot;.
+			They also begin streaming before the event is live, though quickly
+			shut down by Reddit admins.
+			<br />
+			<br />
+			<strong>April 2019 - Sequence</strong>
+			<br />
+			During sequence, users upload and vote on gifs. The most upvoted
+			gifs are strung together into a video. The Snakeroom creates Sneknet
+			V2. The new extension accepts a link to a gif on the backend and
+			then selects this gif for all users running the extension, locking
+			in gif choices without chancing success on the whims of Reddit.
+			<br />
+			<br />
+			<strong>April 2018 - Circle of Trust</strong>
+			<br />
+			In Circle of Trust, users create a circle. Each circle has a
+			passcode, a user not in the circle can use the passcode to join or
+			betray. Joining enlarges the circle, while betraying ends it. The
+			Snakeroom creates the first Sneknet, a browser extension to automate
+			the event. The extension automatically joins a circle from a command
+			sent on the backend and adds anyone running the extension before it
+			can be betrayed.
+			<br />
+			<br />
+			<strong>March 2018 - Snakeroom Genesis</strong>
+			<br />A group founds The Snakeroom Alliance after Reddit discovers
+			r/snakeroomtest and many believe it to be part of the 2018 Reddit
+			Fools’ event. The Snakeroom Alliance is shortened to The Snakeroom
+			and a development team forms to interact with the upcoming event.
 		</Body>
 	</>
 );
