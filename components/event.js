9cabfc629bfc0cc6c6311b23509925a43019489a
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-03-01 10:30:14 -0500

    Add content to the history page

---
+++ components/event.js
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const EventContainer = styled.div`
+	padding: 12px;
+	font-size: 1.1em;
+
+	background: ${(props) => props.theme.colors.backgroundMuted};
+	border-radius: 8px;
+
+	& > h3 {
+		text-align: center;
+		margin-bottom: 0.4em;
+		font-size: 1.5em;
+	}
+`;
+
+const Event = ({ icon, name, time, children }) => (
+	<EventContainer>
+		<h3>
+			<FontAwesomeIcon icon={icon} />
+			{icon && (name || time) ? "\xA0" : ""}
+			<span>
+				{name}
+				{name && time ? ` - ${time}` : ""}
+			</span>
+		</h3>
+		<p>{children}</p>
+	</EventContainer>
+);
+export default Event;
