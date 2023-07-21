bf55d7ef351eb5f82d3284cd30deece03bc4bf5f
haykam821 <24855774+haykam821@users.noreply.github.com>
2023-07-21 17:10:39 -0400

    Sort project members by role and username

--- components/projects/members-row.js
+++ components/projects/members-row.js
@@ -6,15 +6,26 @@ function isNonEmptyArray(array) {
 }
 
 const roles = new Map()
-	.set("owner", "Owner")
-	.set("manager", "Manager")
-	.set("user", "User");
+	.set("owner", {
+		name: "Owner",
+		priority: 0,
+	})
+	.set("manager", {
+		name: "Manager",
+		priority: 1,
+	})
+	.set("user", {
+		name: "User",
+		priority: 2,
+	});
 
 function MemberCard({ member }) {
 	return (
 		<Card>
 			<h4>{member.username}</h4>
-			<InputBlock>Role: {roles.get(member.role)}</InputBlock>
+			<InputBlock>
+				Role: {roles.get(member.role)?.name ?? member.role}
+			</InputBlock>
 		</Card>
 	);
 }
@@ -27,9 +38,21 @@ export default function MembersRow({ members }) {
 	return (
 		<>
 			<h3>Members</h3>
-			{members.map((member) => (
-				<MemberCard key={member.uid} member={member} />
-			))}
+			{members
+				.sort((a, b) => {
+					let sort =
+						(roles.get(a.role)?.priority ?? 0) -
+						(roles.get(b.role)?.priority ?? 0);
+					if (sort !== 0) return sort;
+
+					sort = a.username.localeCompare(b.username);
+					if (sort !== 0) return sort;
+
+					return a.uid.localeCompare(b.uid);
+				})
+				.map((member) => (
+					<MemberCard key={member.uid} member={member} />
+				))}
 		</>
 	);
 }
